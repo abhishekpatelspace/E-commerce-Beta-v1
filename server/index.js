c67c1bc07c092e003cd5d92b4a4dda2c56d16831
abhishekpatelspace <abhishekpatelspace@gmail.com>
2026-07-18 17:42:15 +0530

    Fix firebase-admin cert initialization helper

--- server/index.js
+++ server/index.js
@@ -137,7 +137,7 @@ if (hasFirebaseCreds) {
   try {
     const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
     firebaseAdminApp = admin.initializeApp({
-      credential: admin.credential.cert({
+      credential: admin.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: privateKey
